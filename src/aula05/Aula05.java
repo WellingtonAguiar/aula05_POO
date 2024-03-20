43c97fd0aefc03941e474f9dc3fec3f3a81b4c08
Wellington Aguiar <wellingtonasaguiar@gmail.com>
2024-03-20 10:44:59 -0300

    aula git POO

---
+++ src/aula05/Aula05.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula05;
+
+/**
+ *
+ * @author ads2321036
+ */
+public class Aula05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
